db6de48e4f602cb508697540d343a5e92f765f3d
Diwakar_Rai <rdiwakark23@gmail.com>
2026-01-18 22:59:57 +0530

    database connection done

---
+++ src/lib/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Mongo db URI not defined");
+}
+
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+let cached: MongooseCache = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!, { bufferCommands: false });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
