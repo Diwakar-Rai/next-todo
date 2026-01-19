3c598d8fe2dc1d809182978eb275d9072eed72c6
Diwakar_Rai <rdiwakark23@gmail.com>
2026-01-19 11:53:26 +0530

    schema for the User done

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,36 @@
 import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: ["ACTIVE", "BLOCKED"],
+      default: "ACTIVE",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
